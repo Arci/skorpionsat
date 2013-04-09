e2d08a2035b6c9ea11340775c2f180d6c680e618
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2013-04-09 23:40:17 +0200

    aggiunto gitignore

--- admin/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//Site paths
-define("IMAGES_PATH", "img/");
-define("CSS_PATH", "css/");
-define("JS_PATH", "js/");
--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Site paths
-define("ADMIN_ROOT", "admin/");
-define("IMAGES_PATH", "img/");
-define("CSS_PATH", "css/");
-define("JS_PATH", "js/");
-define("SLIDESHOW_PATH", "slideshow/");
-define("DOCUMENTS_PATH", "documents/");
