f0e54cffff55065ebe74a6a05a3853ea7546ecdd
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2013-04-10 11:55:51 +0200

    migliorata pagina "gioca con noi"

--- play.php
+++ play.php
@@ -15,19 +15,30 @@ buildBottomPage();
 function buildContent(){
     ?>
     <div id="play-container">
-        <div id="play" class="right">
-            <img class="keep-calm right" src="img/play_with_us.jpg" />
-            <!-- TODO contenuto -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor semper scelerisque. Donec dui lacus, scelerisque eu sollicitudin et, condimentum quis diam. Morbi libero tortor, congue non feugiat in, luctus vitae odio. Donec risus mi, pulvinar vel aliquet in, volutpat eu erat. In eget sem leo, vitae scelerisque lorem. Nam aliquam lacinia ante ut mollis. Aliquam erat volutpat.</p>
-            <p>Nunc malesuada varius risus et blandit. Nullam vel turpis ipsum, eu congue eros. Duis malesuada turpis quis ante scelerisque quis cursus orci elementum. Donec eleifend scelerisque luctus. Mauris vitae metus est, a pellentesque nulla. Donec mollis suscipit sodales. Sed luctus facilisis elementum. Curabitur felis nisi, ullamcorper sed malesuada nec, mollis gravida massa. Nunc felis arcu, aliquet at gravida ac, euismod at ante. Proin ante lorem, feugiat molestie commodo sed, fermentum vitae ante. Integer fermentum rutrum magna, eget faucibus ligula lobortis ac. Aliquam tristique scelerisque massa vestibulum facilisis. Vestibulum laoreet mauris et purus porta sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <div id="play-left" class="left">
+            <div id="download" >
+                <span>Download:</span>
+                <div class="first"><a href="<?php echo DOCUMENTS_PATH."Domanda_di_Ammissione.pdf"?>"><img src="img/download.png" class="download-thumbnail" /><span>Domanda di ammissione</span></a></div>
+                <div><a href="<?php echo DOCUMENTS_PATH."Mod.Autorizzazione_Trattamento_Dati_Personali.pdf"?>"><img src="img/download.png" class="download-thumbnail" /><span>Autorizzazione al trattamento dei dati personali</span></a></div>
+                <div><a href="<?php echo DOCUMENTS_PATH."Regolamento_Interno.pdf"?>"><img src="img/download.png" class="download-thumbnail" /><span>Regolamento interno</span></a></div>
+                <div><a href="<?php echo DOCUMENTS_PATH."Scarico_Responsabilita_Minori.pdf"?>"><img src="img/download.png" class="download-thumbnail" /><span>Scarico di responsabilit&agrave per minori</span></a></div>
+                <div><a href="<?php echo DOCUMENTS_PATH."Scarico_Responsabilita.pdf"?>"><img src="img/download.png" class="download-thumbnail" /><span>Scarico di responsabilit&agrave</span></a></div>
+            </div>
+            <div id="contact">
+                <span>Contatti:</span>
+                <div><a href="mailto:presidente@skorpionsat.com">Presidente</a></div>
+                <div><a href="mailto:segretario@skorpionsat.com">Segretario</a></div>
+                <div><a href="mailto:noleggi@skorpionsat.com">Responsabile noleggi</a></div>
+                <div><img src="img/phone.png" class="phone-thumbnail" />3287526556</div>
+            </div> 
         </div>
-        <div id="download" class="left">
-            <span>Download:</span>
-            <div class="first"><a href="documents/Domanda_di_Ammissione.pdf"><img src="img/download.png" class="download-thumbnail" /><span>Domanda di ammissione</span></a></div>
-            <div><a href="documents/Mod.Autorizzazione_Trattamento_Dati_Personali.pdf"><img src="img/download.png" class="download-thumbnail" /><span>Autorizzazione al trattamento dei dati personali</span></a></div>
-            <div><a href="documents/Regolamento_Interno.pdf"><img src="img/download.png" class="download-thumbnail" /><span>Regolamento interno</span></a></div>
-            <div><a href="documents/Scarico_Responsabilita_Minori.pdf"><img src="img/download.png" class="download-thumbnail" /><span>Scarico di responsabilit&agrave per minori</span></a></div>
-            <div><a href="documents/Scarico_Responsabilita.pdf"><img src="img/download.png" class="download-thumbnail" /><span>Scarico di responsabilit&agrave</span></a></div>
+        <div id="play-right" class="right">
+            <div id="play" class="right">
+                <!-- TODO contenuto -->
+                <img class="keep-calm right" src="img/play_with_us.jpg" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor semper scelerisque. Donec dui lacus, scelerisque eu sollicitudin et, condimentum quis diam. Morbi libero tortor, congue non feugiat in, luctus vitae odio. Donec risus mi, pulvinar vel aliquet in, volutpat eu erat. In eget sem leo, vitae scelerisque lorem. Nam aliquam lacinia ante ut mollis. Aliquam erat volutpat.</p>
+                <p>Nunc malesuada varius risus et blandit. Nullam vel turpis ipsum, eu congue eros. Duis malesuada turpis quis ante scelerisque quis cursus orci elementum. Donec eleifend scelerisque luctus. Mauris vitae metus est, a pellentesque nulla. Donec mollis suscipit sodales. Sed luctus facilisis elementum. Curabitur felis nisi, ullamcorper sed malesuada nec, mollis gravida massa. Nunc felis arcu, aliquet at gravida ac, euismod at ante. Proin ante lorem, feugiat molestie commodo sed, fermentum vitae ante. Integer fermentum rutrum magna, eget faucibus ligula lobortis ac. Aliquam tristique scelerisque massa vestibulum facilisis. Vestibulum laoreet mauris et purus porta sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
         </div>
         <div class="clear"></div>
     </div>
